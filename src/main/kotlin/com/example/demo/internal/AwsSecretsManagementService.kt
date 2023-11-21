09f604729453875653734bc7b8fbaa49243b9f9f
Hantsy Bai <hantsy@gmail.com>
2023-11-21 15:13:42 +0800

    Add SecretsManager Example (#7)
    * chore: fix conflicts
    
    * chore: clean codes

---
+++ src/main/kotlin/com/example/demo/internal/AwsSecretsManagementService.kt
@@ -0,0 +1,51 @@
+package com.example.demo.internal
+
+import aws.sdk.kotlin.services.secretsmanager.SecretsManagerClient
+import aws.sdk.kotlin.services.secretsmanager.model.CreateSecretRequest
+import aws.sdk.kotlin.services.secretsmanager.model.GetSecretValueRequest
+import com.example.demo.SecretsManagementService
+import com.example.demo.SecretsManagementServiceException
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class AwsSecretsManagementService(
+    private val client: SecretsManagerClient
+) : SecretsManagementService {
+    companion object {
+        private val log = LoggerFactory.getLogger(SecretsManagementService::class.java)
+    }
+
+    override suspend fun write(secretName: String, secretValue: String, secretDescription: String?): String? {
+        val request = CreateSecretRequest {
+            name = secretName
+            description = secretDescription
+            secretString = secretValue
+            forceOverwriteReplicaSecret = true // override the duplicated secret name
+        }
+
+        log.debug("writing secrets: $secretName, $secretValue, $secretDescription")
+        val response = try {
+            client.createSecret(request)
+        } catch (e: Exception) {
+            log.error("error occurred when writing secrets: ${e.message}")
+            throw SecretsManagementServiceException(e.message ?: "Failed to write secrets:")
+        }
+        return response.arn
+    }
+
+    override suspend fun read(secretName: String): String? {
+        val valueRequest = GetSecretValueRequest {
+            secretId = secretName
+        }
+
+        log.debug("read secrets by name: $secretName")
+        val response = try {
+            client.getSecretValue(valueRequest)
+        } catch (e: Exception) {
+            log.error("error occurred when reading secrets: ${e.message}")
+            throw SecretsManagementServiceException(e.message ?: "Failed to read secrets")
+        }
+        return response.secretString
+    }
+}
---
+++ src/test/kotlin/com/example/demo/AwsSecretsManagementServiceTest.kt
@@ -0,0 +1,42 @@
+package com.example.demo
+
+import com.example.demo.AwsSecretsManagementServiceTest.AwsSecretsManagementServiceTestConfig
+import com.example.demo.internal.AwsConfig
+import com.example.demo.internal.AwsProperties
+import com.example.demo.internal.AwsSecretsManagementService
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+
+@SpringBootTest(
+    classes = [AwsSecretsManagementServiceTestConfig::class],
+    properties = ["aws.endpoint=http://localhost:4566"]
+)
+class AwsSecretsManagementServiceTest {
+    companion object {
+        private val log = LoggerFactory.getLogger(AwsSecretsManagementServiceTest::class.java)
+    }
+
+    @Configuration
+    @Import(AwsSecretsManagementService::class, AwsConfig::class)
+    @EnableConfigurationProperties(AwsProperties::class)
+    class AwsSecretsManagementServiceTestConfig
+
+    @Autowired
+    lateinit var client: SecretsManagementService
+
+    @Test
+    fun `read and write secrets`() = runTest {
+        val arn = client.write("/mysecrests/test", "test", "")
+        log.debug("writing secrets, return arn: $arn")
+        val secretVal = client.read("/mysecrests/test")
+        secretVal shouldBe "test"
+    }
+}
