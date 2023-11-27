20f24d31217c3031581c9ff52fb120c6366bf1f4
hantsy <hantsy@gmail.com>
2023-11-27 15:49:04 +0800

    chore: clean codes

--- src/main/kotlin/com/example/demo/internal/AwsProperties.kt
+++ src/main/kotlin/com/example/demo/internal/AwsProperties.kt
@@ -5,28 +5,29 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 const val DEFAULT_BUCKET_NAME = "default"
 const val DEFAULT_SQS_QUEUE_NAME = "default"
+const val DEFAULT_REGION = "us-east-1"
 
 @ConfigurationProperties(prefix = "aws")
 data class AwsProperties(
     val accessKeyId: String,
     val secretAccessKey: String,
-    val region: String="us-east-1",
+    val region: String = DEFAULT_REGION,
     val s3: S3Properties? = null,
     val secretsManager: SecretsManagerProperties? = null,
     val sqs: SqsProperties? = null
 ) {
     data class S3Properties(
         val bucketName: String = DEFAULT_BUCKET_NAME,
-        val endpoint: String? = "http://localhost:4566"
+        val endpoint: String?
     )
 
     data class SecretsManagerProperties(
-        val endpoint: String? = "http://localhost:4566"
+        val endpoint: String?
     )
 
     data class SqsProperties(
         val queueName: String = DEFAULT_SQS_QUEUE_NAME,
-        val endpoint: String? = "http://localhost:4566"
+        val endpoint: String?
     )
 
 }
