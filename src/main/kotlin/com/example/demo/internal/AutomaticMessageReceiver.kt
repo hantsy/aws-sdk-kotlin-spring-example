aff8bb033530ad9a478a517dfe6369c67bdc24d2
hantsy <hantsy@gmail.com>
2023-11-27 17:05:56 +0800

    chore: clean codes

---
+++ src/main/kotlin/com/example/demo/internal/AutomaticMessageReceiver.kt
@@ -0,0 +1,21 @@
+package com.example.demo.internal
+
+import com.example.demo.MessagingService
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+class AutomaticMessageReceiver(val messagingService: MessagingService) {
+    companion object {
+        private val log = LoggerFactory.getLogger(AutomaticMessageReceiver::class.java)
+    }
+
+    @Scheduled(initialDelay = 500, fixedRate = 100)
+    suspend fun onMessageReceived() {
+        messagingService.receive(DEFAULT_SQS_QUEUE_NAME, String::class.java)
+            .forEach {
+                log.debug("received messaging service: $it")
+            }
+    }
+}
